ade757fdd2d2bf4b96c99c0ef936b6034b478793
Dhanush Babu K <103583233+dhanushbabuk@users.noreply.github.com>
2024-05-30 23:58:20 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 3000;
 export const mongodbURl =
-  "mongodb+srv://bookstore:dhanush786@cluster0.0hec6ej.mongodb.net/books-collection?retryWrites=true&w=majority";
+  "your Url";
